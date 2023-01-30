bda4d1e7d594e4b50d040d7e1581f6e5898d2e7d
Michael <12646562+mbround18@users.noreply.github.com>
2023-01-30 10:35:40 -0600

    auto into actionyml

--- scripts/exec_auto.sh
+++ scripts/exec_auto.sh
@@ -1,14 +1,2 @@
 #!/usr/bin/env bash
 
-echo "::group::Run Auto"
-set -o pipefail
-
-OUTPUT_FILE="/tmp/auto.out"
-
-
-exec "auto ${1}" 2>&1 | tee "${OUTPUT_FILE}"
-
-AUTO_EXIT="$?"
-
-echo "::endgroup::"
-exit $AUTO_EXIT
