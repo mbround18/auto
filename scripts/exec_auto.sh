c0241923ee1afdb72a9640edb0467188d6d760c8
Michael <12646562+mbround18@users.noreply.github.com>
2023-01-30 10:27:52 -0600

    exec auto

--- scripts/exec_auto.sh
+++ scripts/exec_auto.sh
@@ -4,9 +4,9 @@ echo "::group::Run Auto"
 set -o pipefail
 
 OUTPUT_FILE="/tmp/auto.out"
-OPTIONS=(${1})
 
-auto "${OPTION[@]}" 2>&1 | tee "${OUTPUT_FILE}"
+
+exec "auto ${1}" 2>&1 | tee "${OUTPUT_FILE}"
 
 AUTO_EXIT="$?"
 
